ecca23c28e0a75b41ae170fba6a42b5fa4d4c1e5
高先生 <106066550+gao324@users.noreply.github.com>
2022-06-28 20:41:17 +0800

    项目初始化

---
+++ src/api/user.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+  })
+}
+
+export function getInfo(token) {
+  return request({
+
+  })
+}
+
+export function logout() {
+  return request({
+
+  })
+}
